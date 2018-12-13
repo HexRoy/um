5ba6b7b4a327325dd5fd50218d10f911887121ff
Liam Phelan <liamp408@my.uri.edu>
2018-12-13 16:34:09 -0500

    added instruction.h

---
+++ instruction.h
@@ -0,0 +1,49 @@
+#ifndef INSTRUCTION_INCLUDED
+#define INSTRUCTION_INCLUDED
+#include <math.h>
+#include <stdio.h>
+#include "segment.h"
+#include "execUM.h"
+//OPCODE 0
+void conmove(uint32_t a, uint32_t b, uint32_t c, uint32_t *r);
+
+//OPCODE 1
+void segload(uint32_t a, uint32_t b, uint32_t c, uint32_t *r, Seg_T segment_memory);
+
+//OPCODE 2
+void segstore(uint32_t a, uint32_t b, uint32_t c, uint32_t *r, Seg_T segment_memory);
+
+//OPCODE 3
+void add(uint32_t a, uint32_t b, uint32_t c, uint32_t *r);
+
+//OPCODE 4
+void mult(uint32_t a, uint32_t b, uint32_t c, uint32_t *r);
+
+//OPCODE 5
+void div(uint32_t a, uint32_t b, uint32_t c, uint32_t *r);
+
+//OPCODE 6
+void nand(uint32_t a, uint32_t b, uint32_t c, uint32_t *r);
+
+//OPCODE 7
+void halt();
+
+//OPCODE 8
+void mapseg(uint32_t b, uint32_t c, uint32_t *r , Seg_T segment_memory);
+
+//OPCODE 9
+void unmapseg(uint32_t c, uint32_t *r, Seg_T segment_memory);
+
+//OPCODE 10
+void output(uint32_t c, uint32_t *r);
+
+//OPCODE 11
+void input(uint32_t c, uint32_t *r);
+
+//OPCODE 12
+void load(uint32_t b, uint32_t c, uint32_t *r, Seg_T segment_memory);
+
+//OPCODE 13
+void loadval(uint32_t a, uint32_t val, uint32_t *r);
+
+#endif
